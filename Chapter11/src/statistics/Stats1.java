01a0c93a854f21479a52c243ef5c1ad0aecfec92
904275012 <904275012@L815WD220019.edu.cbe.ab.ca>
2022-04-08 10:53:16 -0600

    April 8

---
+++ Chapter11/src/statistics/Stats1.java
@@ -0,0 +1,10 @@
+package statistics;
+
+public class Stats1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Chapter11/src/theFiles/MyFile2.java
+++ Chapter11/src/theFiles/MyFile2.java
@@ -1,6 +1,6 @@
 /*
 
-Program: MyFile2.java          Last Date of this Revision: April 7, 2022
+Program: MyFile2.java          Last Date of this Revision: April 8, 2022
 
 Purpose: Create a MyFile application that creates a file named zzz.txt and then displays a message indicating that the file has been created. The application should 
 prompt the the user to either keep or delete the file. If the file is deleted, a message should notify the user when the file has been successfully deleted
@@ -27,13 +27,51 @@ public class MyFile2 {
 		}
 		else {
 
-			do {
+			try{
 				textFile.createNewFile();
 				System.out.println("New file has been created");
+				
+				Scanner ask = new Scanner(System.in);
+				System.out.println("Would you like to keep or delete your file? 1 for keep, 2 for delete");
+				int ans = ask.nextInt();
+				
+				if(ans ==1) {
+					System.out.println("The file was saved");
+				}
+				if(ans == 2) {
+					textFile.delete();
+					System.out.println("The file has been deleted");
+				}
+			}
+			catch (FileNotFoundException e) {
+				System.out.println("File does not exist or could not be found.");
+				System.err.println("FileNotFoundException: " + e.getMessage());
+			} catch (IOException e) {
+				System.out.println("Problem reading file.");
+	    		System.err.println("IOException: " + e.getMessage());
+	    	}
+				
 			}
 			
 			
 		}
 	}
 
-}
+/*
+ Screen dump
+ 
+The file already exists
+
+
+New file has been created
+Would you like to keep or delete your file? 1 for keep, 2 for delete
+2
+The file has been deleted
+
+ New file has been created
+Would you like to keep or delete your file? 1 for keep, 2 for delete
+1
+The file was saved
+
+ */
+
