926733caaddf49a4fef8ffe78364a71ffd9193c2
904275012 <904275012@L815WD220019.edu.cbe.ab.ca>
2022-04-21 10:22:38 -0600

    april 21

--- Chapter11/src/theRoster/Roster.java
+++ Chapter11/src/theRoster/Roster.java
@@ -1,9 +1,24 @@
 package theRoster;
 
+
+import java.io.*;
+import java.util.Scanner;
+
 public class Roster {
 
 	public static void main(String[] args) {
 		
+		File dataFile = new File("StuRoster.txt");
+		
+		FileWriter out;
+		BufferedWriter writeFile;
+		Scanner input = new Scanner(System.in);
+		String Names;
+		
+		try {
+			out = new FileWriter(dataFile);
+			writeFile = new BufferedWriter(out);
+		}
 
 	}
 
--- Chapter11/src/theRoster/StuName.java
+++ Chapter11/src/theRoster/StuName.java
@@ -1,10 +1,18 @@
 package theRoster;
 
 public class StuName {
+	
+	private String first, last;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public String firstName() {
+		return(first);
 	}
 
+	public String lastName() {
+		return(last);
+	}
+	
+	public String toString() {
+		return("First name is: " + firstName() + " and last name is: " + lastName());
+	}
 }
