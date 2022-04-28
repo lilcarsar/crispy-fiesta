fae9098691ce8518412662990f0fbd0a64470b58
904275012 <904275012@L815WD220019.edu.cbe.ab.ca>
2022-04-28 10:24:33 -0600

    april 28

--- Chapter11/src/replaceAndFind/FindAndReplace.java
+++ Chapter11/src/replaceAndFind/FindAndReplace.java
@@ -1,10 +1,55 @@
+/*
+
+Program: FindAndReplace.java          Last Date of this Revision: April 28, 2022
+
+Purpose: Create a FindAndReplace application that prompts the user for a file name, a search word or phrase, FindAndReplace finds all occurrences
+of the search word or phrase in a file and replaces them with the specified replacement word or phrase
+
+Author: Carter Sarney
+School: CHHS
+Course: Computer Programming 30
+ 
+
+*/
+
 package replaceAndFind;
+
+import java.io.*;
+import java.util.*;
 
 public class FindAndReplace {
 
 	public static void main(String[] args) {
 		
+		
+		String fileName; 
+		String find;
+		String replace;
 
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.print("Enter the name of the test file: ");
+		fileName = input.nextLine();
+		File name = new File(fileName);
+		
+		System.out.println("What phrase or search word are you looking for? ");
+		find = input.nextLine();
+		
+		System.out.println("What replacement search word or phrase do you want? ");
+		replace = input.nextLine();
+		
+		
+		
+		try {
+			
+			
+		}catch (FileNotFoundException e) {
+			System.out.println("File does not exist or could not be found.");
+			System.err.println("FileNotFoundException: " + e.getMessage());
+		}
+		
 	}
 
 }
