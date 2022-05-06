ce9c89715af6df9cdd1aef315afb0f08ad59bea8
904275012 <904275012@L815WD220019.edu.cbe.ab.ca>
2022-05-06 10:34:47 -0600

    may 6

--- Chapter9/src/topGrades/HighestGrade.java
+++ Chapter9/src/topGrades/HighestGrade.java
@@ -1,9 +1,42 @@
 package topGrades;
 
+
+import java.util.*;
+import java.io.*;
+import java.text.NumberFormat;
+
+
+
 public class HighestGrade {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		Scanner input = new Scanner(System.in);
+		double score;
+		String name;
+		double avgScore;
+		String line;
+		double totalScores = 0;
+		int numScores = 0;
+		double low = 100;
+		double high = 0;
+		double skill = 0;
+
+		
+		
+		ArrayList<String> stuName = new ArrayList<String>();
+		ArrayList<String> stuGrades = new ArrayList<String>();
+		
+		
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println("enter students name: ");
+			name = input.next();	
+			
+			System.out.println("Enter test score");
+			score = input.nextDouble();
+		}
 
 	}
 
