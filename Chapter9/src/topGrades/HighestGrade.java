4d85bca95c3fc5316d0420f4e05e9ab0701e89ee
904275012 <904275012@L815WD220019.edu.cbe.ab.ca>
2022-05-10 10:23:13 -0600

    may 10, done all but local bank and crt

--- Chapter9/src/topGrades/HighestGrade.java
+++ Chapter9/src/topGrades/HighestGrade.java
@@ -11,33 +11,42 @@ public class HighestGrade {
 
 	public static void main(String[] args) {
 		
+		NumberFormat percent = NumberFormat.getPercentInstance();
 		
 		Scanner input = new Scanner(System.in);
-		double score;
-		String name;
-		double avgScore;
-		String line;
-		double totalScores = 0;
-		int numScores = 0;
-		double low = 100;
-		double high = 0;
-		double skill = 0;
 
-		
-		
-		ArrayList<String> stuName = new ArrayList<String>();
-		ArrayList<String> stuGrades = new ArrayList<String>();
+		double high = 0;
+
+		int[] grades = new int[5];
 		
 		
 		
 		for(int i = 0; i < 5; i++) {
-			System.out.println("enter students name: ");
-			name = input.next();	
+				
 			
-			System.out.println("Enter test score");
-			score = input.nextDouble();
+			System.out.println("Enter grade: ");
+			grades[i] = input.nextInt();
+		
+		
+		if(grades[i] > high) {
+			high = grades[i];
+		}
 		}
 
+		System.out.println("The highest grade is " + percent.format(high/100));
 	}
 
 }
+/* Screen Dump
+Enter grade: 
+96
+Enter grade: 
+56
+Enter grade: 
+19
+Enter grade: 
+75
+Enter grade: 
+84
+The highest grade is 96%
+ */
